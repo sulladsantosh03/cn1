6dbe292016c2084138d699096f96e40e6159e52c
sulladsantosh03 <sulladsantosh03@gmail.com>
2024-11-21 09:50:50 +0530

    Create cn3.tcl

---
+++ cn3.tcl
@@ -0,0 +1,67 @@
+[3]
+1.Program title:
+Design networks as shown in figure 2 that demonstrate the working of Distance vector routing
+protocol.
+The link between node 1 and 4 breaks at 1.0 ms and comes up at 3.0ms. Assume that the source
+node 0 transmits packets to node 4. Plot the congestion window when TCP sends packets via other
+nodes. Assume your own parameters for bandwidth and delay.
+Fig 2: Network Topology
+2. Script executed in lab.
+set ns [new Simulator]
+set tf [open ex3.tr w]
+$ns trace-all $tf
+set nf [open ex3.nam w]
+$ns namtrace-all $nf
+set cwind [open win3.tr w]
+$ns color 1 Blue
+$ns color 2 Red
+$ns rtproto DV
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+$ns duplex-link $n0 $n1 0.3Mb 10ms DropTail
+$ns duplex-link $n0 $n2 0.3Mb 10ms DropTail
+$ns duplex-link $n2 $n3 0.3Mb 10ms DropTail
+$ns duplex-link $n1 $n4 0.3Mb 10ms DropTail
+$ns duplex-link $n3 $n5 0.5Mb 10ms DropTail
+$ns duplex-link $n4 $n5 0.5Mb 10ms DropTail
+$ns duplex-link-op $n0 $n1 orient right-up
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n2 $n3 orient right
+$ns duplex-link-op $n1 $n4 orient right
+$ns duplex-link-op $n3 $n5 orient right-up
+$ns duplex-link-op $n4 $n5 orient right-down
+set tcp [new Agent/TCP]
+$ns attach-agent $n0 $tcp
+set sink [new Agent/TCPSink]
+$ns attach-agent $n4 $sink
+$ns connect $tcp $sink
+$tcp set fid_ 1
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ns rtmodel-at 1.0 down $n1 $n4
+$ns rtmodel-at 3.0 up $n1 $n4
+$ns at 0.1 "$ftp start
+$ns at 12.0 "finish"
+proc plotWindow {tcpSource file} {
+global ns
+set time 0.01
+set now [$ns now]
+set cwnd [$tcpSource set cwnd_]
+puts $file "$now $cwnd"
+$ns at [expr $now + $time] "plotWindow $tcpSource $file"
+}
+$ns at 1.0 "plotWindow $tcp $cwind"
+proc finish {} {
+global ns tf nf cwind
+$ns flush-trace
+close $tf
+close $nf
+exec nam ex3.nam &
+exec xgraph win3.tr &
+exit 0
+}
+$ns run
